9315a6b6d1553293c67342c5af6b24ef2b17d30c
Shunchao Wang <shunchao.wang@gmail.com>
2019-12-06 12:59:55 -0500

    Added App page.

---
+++ src/Pages/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+};
+
+export default App;
